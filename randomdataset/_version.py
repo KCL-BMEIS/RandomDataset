7c5cbe61e42defe00278662e0e89fbe712f8cd59
Eric Kerfoot <17726042+ericspod@users.noreply.github.com>
2021-04-20 18:28:30 +0100

    ExeTera Example and Version Changes (#8)
    * Update randomdataset.rst
    
    * Update index.rst
    
    * Update conf.py and index.rst
    
    * Update .gitignore, conf.py, and 3 more files...
    
    * File updates and restructuring
    
    * Update generator.py
    
    * Updates and reorganization
    
    * Adding notebook for Exetera example
    
    * Update exeteraschema.json
    
    * Added Exetera example
    
    * CI/CD fix
    
    * Cleanup
    
    * Adding Codecov support
    
    * Update README.md
    
    * Tweak
    
    * Adding documentation
    
    * Adding Documentation
    
    * Adding commentary
    
    * Notebook update
    
    * Remove path line
    
    * Update
    
    * Added merge example
    
    * Adding examples
    
    * Fix
    
    * Added _version.py file for clean versioning in setup.py
    
    This avoids importing the package in the setup.py file, which would fail if dependencies are missing
    
    * Fix

---
+++ randomdataset/_version.py
@@ -0,0 +1,10 @@
+# RandomDataset
+# Copyright (c) 2021 Eric Kerfoot, KCL, see LICENSE file
+
+__appname__ = "RandomDataset"
+__version_info__ = (0, 1, 0)  # global application version, major/minor/patch
+__version__ = "%i.%i.%i" % __version_info__
+__description__ = "Random dataset generation tool"
+__author__ = "Eric Kerfoot, KCL"
+__author_email__ = "eric.kerfoot@kcl.ac.uk"
+__copyright__ = "Copyright (c) 2021 Eric Kerfoot, King's College London, all rights reserved. Licensed under MIT License (see LICENSE.txt)."
